16ccc895e7872b5652a854d8120d5d8e3d6ff4aa
Milan Iliev <milaniliev@gmail.com>
2016-03-01 16:30:10 -0500

    Increase Nav poll rate 10x

--- ball_bot/inertial_navigation.py
+++ ball_bot/inertial_navigation.py
@@ -6,7 +6,7 @@ import json
 
 class InertialNavigation:
   calibration_file_path = 'calibration.json'
-  update_interval = 0.1 # seconds
+  update_interval = 0.01 # seconds
   
   def __init__(self):
     self.sensors = BNO055.BNO055(serial_port='/dev/ttyAMA0', rst=18)
